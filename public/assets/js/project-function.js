5724e063c6595ba9334bc59b30065b4aaf1439b4
unknown <dickensallinone@gmail.com>
2025-02-07 18:46:38 +0800

    add drag and drop functionality

--- public/assets/js/project-function.js
+++ public/assets/js/project-function.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
-
     $(".child").draggable({
-        revert:true
-    });
-
-    $(".child--1").draggable({
-        revert:true
-    });
-
-    $(".child--2").draggable({
-        revert:true
-    });
-
-    $(".parent").droppable({
-        accept: '.child',
-        drop: function(event, ui) {
-          $(this).append($(ui.draggable));
+        revert: "invalid",  // Prevent snapping back on valid drop
+        start: function(event, ui) {
+            $(this).css("opacity", "1"); // Reduce opacity while dragging
+        },
+        stop: function(event, ui) {
+            $(this).css("opacity", "1"); // Restore opacity after drop
         }
-      });
+    });
+    
+    $(".parent").droppable({
+        accept: ".child",
+        drop: function(event, ui) {
+            let $dragged = $(ui.draggable);
+    
+            // Move to the new parent with animation
+            $dragged.css({ position: "relative", top: 0, left: 0 })
+                .appendTo($(this))
+                .hide()
+                .fadeIn(300);
+    
+            // Adjust the spacing of all children in the new parent smoothly
+            let children = $(this).find(".child");
+            children.each(function(index) {
+                $(this).animate({ marginTop: "5px" }, 300);
+            });
+        }
+    });
+    
   
-
-    $("#save-changes").click(function(){
-        console.log("Helo here");
-    })
     
 })
