a2ed3529cfd7a9d55d5b7adb975835b36edb51d6
unknown <dickensallinone@gmail.com>
2025-02-27 21:40:14 +0800

    add new changes on the front end jquery

--- public/assets/js/project-function.js
+++ public/assets/js/project-function.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
-    $(".child").draggable({
-        revert: "invalid",  // Prevent snapping back on valid drop
-        start: function(event, ui) {
-            $(this).css("opacity", "1"); // Reduce opacity while dragging
-        },
-        stop: function(event, ui) {
-            $(this).css("opacity", "1"); // Restore opacity after drop
-        }
-    });
+    // $(".child").draggable({
+    //     revert: "invalid",  // Prevent snapping back on valid drop
+    //     start: function(event, ui) {
+    //         $(this).css("opacity", "1"); // Reduce opacity while dragging
+    //     },
+    //     stop: function(event, ui) {
+    //         $(this).css("opacity", "1"); // Restore opacity after drop
+    //     }
+    // });
     
-    $(".parent").droppable({
-        accept: ".child",
-        drop: function(event, ui) {
-            let $dragged = $(ui.draggable);
+    // $(".parent").droppable({
+    //     accept: ".child",
+    //     drop: function(event, ui) {
+    //         let $dragged = $(ui.draggable);
     
-            // Move to the new parent with animation
-            $dragged.css({ position: "relative", top: 0, left: 0 })
-                .appendTo($(this))
-                .hide()
-                .fadeIn(300);
+    //         // Move to the new parent with animation
+    //         $dragged.css({ position: "relative", top: 0, left: 0 })
+    //             .appendTo($(this))
+    //             .hide()
+    //             .fadeIn(300);
     
-            // Adjust the spacing of all children in the new parent smoothly
-            let children = $(this).find(".child");
-            children.each(function(index) {
-                $(this).animate({ marginTop: "5px" }, 300);
-            });
-        }
-    });
+    //         // Adjust the spacing of all children in the new parent smoothly
+    //         let children = $(this).find(".child");
+    //         children.each(function(index) {
+    //             $(this).animate({ marginTop: "5px" }, 300);
+    //         });
+    //     }
+    // });
 
 
     $('#project-form').on('submit', function (event) {
         event.preventDefault(); // Prevent the default form submission
   
-        $("#project-modal--form").show();
-  
+        $("#project-creation--loader-container").show();
         // if($(".message").length > 0) {
         //   $(".message").remove();
         // } 
